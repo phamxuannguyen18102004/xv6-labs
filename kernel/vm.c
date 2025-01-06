5e35b67a1120cedde5f624355110a78e8a7e7107
TinyDragons <minhtanpham379@gmail.com>
2025-01-06 23:46:58 +0700

    Done Lab 3 - print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void print_pte(pagetable_t pagetable, int level)
+{
+  for (int i=0; i<512; i++) {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      uint64 child = PTE2PA(pte);
+      // print prefix ..
+      for (int j =0; j<level; j++)
+        printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      // if not leaf, print next level
+      if ((pte&(PTE_R|PTE_W|PTE_X)) == 0)
+        print_pte((pagetable_t)child, level+1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  print_pte(pagetable, 0);
+}
